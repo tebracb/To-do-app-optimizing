b98c03080a6d04fa07c657ac943f7193ee251a05
tebracb <bori.temesi@gmail.com>
2019-11-08 13:20:11 +0100

    should show active entries test

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -98,6 +98,12 @@ describe('controller', function () {
 		it('should show active entries', function () {
 			// TODO: write test
 
+			var todo = { title: 'my todo', completed: false };
+            setUpModel([todo]);
+
+            subject.setView('');
+            expect(view.render).toHaveBeenCalledWith('showEntries', [todo]);
+
 		});
 
 		it('should show completed entries', function () {
